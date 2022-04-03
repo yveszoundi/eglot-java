eb3f67ad791130af900a75385e81634ccf345c57
Yves Zoundi <yveszoundi@gmail.com>
2022-04-03 12:31:08 -0400

    Update documentation and catch up with latest eglot changes

--- eglot-java.el
+++ eglot-java.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2019-2021 Yves Zoundi
 
-;; Version: 1.3
+;; Version: 1.4
 ;; Author: Yves Zoundi <yves_zoundi@hotmail.com>
 ;; Maintainer: Yves Zoundi <yves_zoundi@hotmail.com>
 ;; URL: https://github.com/yveszoundi/eglot-java
@@ -24,7 +24,12 @@
 
 ;;; Commentary:
 
-;; Java extension for eglot
+;; Java extension for eglot. Some of the key features include the following:
+;; - Automatic installation of the Eclipse JDT LSP server.
+;; - Ability to pass JVM arguments to the Eclipse JDT LSP server (eglot-java-eclipse-jdt-args)
+;; - Wizards for Spring starter, Maven and Gradle project creation
+;; - Generic build command support for Maven and Gradle projects
+;; - JUnit tests support, this hasn't been tested for a while...
 
 ;;
 ;;; Code:
@@ -37,6 +42,12 @@
   "Interaction with a Java language server via eglot."
   :prefix "eglot-java-"
   :group 'eglot)
+
+(defcustom eglot-java-eclipse-jdt-args
+  '()
+  "Eclipse JDT arguments."
+  :type '(repeat string)
+  :group 'eglot-java)
 
 (defcustom eglot-java-eclipse-jdt-ls-download-url
   "https://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz"
@@ -107,6 +118,102 @@
 (declare-function tar-untar-buffer "tar-mode" ())
 (declare-function xml-get-children "xml" (node child-name))
 
+(defclass eglot-eclipse-jdt (eglot-lsp-server) ()
+  :documentation "Eclipse's Java Development Tools Language Server.")
+
+(cl-defmethod eglot-initialization-options ((server eglot-eclipse-jdt))
+  "Passes through required jdt initialization options."
+  `(:workspaceFolders
+    [,@(cl-delete-duplicates
+        (mapcar #'eglot--path-to-uri
+                (let* ((root (project-root (eglot--project server))))
+                  (cons root
+                        (mapcar
+                         #'file-name-directory
+                         (append
+                          (file-expand-wildcards (concat root "*/pom.xml"))
+                          (file-expand-wildcards (concat root "*/build.gradle"))
+                          (file-expand-wildcards (concat root "*/.project")))))))
+        :test #'string=)]
+    ,@(if-let ((home (or (getenv "JAVA_HOME")
+                         (ignore-errors
+                           (expand-file-name
+                            ".."
+                            (file-name-directory
+                             (file-chase-links (executable-find "javac"))))))))
+          `(:settings (:java (:home ,home)))
+        (ignore (eglot--warn "JAVA_HOME env var not set")))))
+
+(defun eglot--eclipse-jdt-contact (interactive)
+  "Return a contact for connecting to eclipse.jdt.ls server, as a cons cell.
+If INTERACTIVE, prompt user for details."
+  (cl-labels
+      ((is-the-jar
+        (path)
+        (and (string-match-p
+              "org\\.eclipse\\.equinox\\.launcher_.*\\.jar$"
+              (file-name-nondirectory path))
+             (file-exists-p path))))
+    (let* ((classpath (or (getenv "CLASSPATH") path-separator))
+           (cp-jar (cl-find-if #'is-the-jar (split-string classpath path-separator)))
+           (jar cp-jar)
+           (dir
+            (cond
+             (jar (file-name-as-directory
+                   (expand-file-name ".." (file-name-directory jar))))
+             (interactive
+              (expand-file-name
+               (read-directory-name
+                (concat "Path to eclipse.jdt.ls directory (could not"
+                        " find it in CLASSPATH): ")
+                nil nil t)))
+             (t (error "Could not find eclipse.jdt.ls jar in CLASSPATH"))))
+           (repodir
+            (concat dir
+                    "org.eclipse.jdt.ls.product/target/repository/"))
+           (repodir (if (file-directory-p repodir) repodir dir))
+           (config
+            (concat
+             repodir
+             (cond
+              ((string= system-type "darwin") "config_mac")
+              ((string= system-type "windows-nt") "config_win")
+              (t "config_linux"))))
+           (workspace
+            (expand-file-name (md5 (project-root (eglot--current-project)))
+                              (locate-user-emacs-file
+                               "eglot-eclipse-jdt-cache"))))
+      (unless jar
+        (setq jar
+              (cl-find-if #'is-the-jar
+                          (directory-files (concat repodir "plugins") t))))
+      (unless (and jar (file-exists-p jar) (file-directory-p config))
+        (error "Could not find required eclipse.jdt.ls files (build required?)"))
+      (when (and interactive (not cp-jar)
+                 (y-or-n-p (concat "Add path to the server program "
+                                   "to CLASSPATH environment variable?")))
+        (setenv "CLASSPATH" (concat (getenv "CLASSPATH") path-separator jar)))
+      (unless (file-directory-p workspace)
+        (make-directory workspace t))
+      (cons 'eglot-eclipse-jdt
+            (nconc
+             (list
+              (executable-find "java")
+              "-Declipse.application=org.eclipse.jdt.ls.core.id1"
+              "-Dosgi.bundles.defaultStartLevel=4"
+              "-Declipse.product=org.eclipse.jdt.ls.core.product")
+             eglot-java-eclipse-jdt-args
+             (list
+              "-jar" jar
+              "-configuration" config
+              "-data" workspace))))))
+
+(cl-defmethod eglot-execute-command
+  ((_server eglot-eclipse-jdt) (_cmd (eql java.apply.workspaceEdit)) arguments)
+  "Eclipse JDT breaks spec and replies with edits as arguments."
+  (mapc #'eglot--apply-workspace-edit arguments))
+
+
 (defun eglot-java--download-file (source-url dest-location)
   "Download a file from a URL at SOURCE-URL and save it to file at DEST-LOCATION."
   (let* ((dest-dir     (file-name-directory dest-location))
@@ -138,6 +245,9 @@ Otherwise returns nil"
                                                             nil
                                                             "^org.eclipse.equinox.launcher_.*.jar$"
                                                             t))))
+    (if (not equinox-launcher-jar)
+        (user-error "Could not find Eclipse OSGI jar launcher!"))
+
     (expand-file-name equinox-launcher-jar
                       lsp-java-server-plugins-dir)))
 
