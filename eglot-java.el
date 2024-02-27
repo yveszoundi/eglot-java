b24b0e1e6fb94b78002f1df37fc3956907c51da7
Magiel Bruntink <magiel.bruntink@icloud.com>
2024-02-27 22:01:45 +0100

    Move junit-platform-console-standalone jar to front of classpath.

--- eglot-java.el
+++ eglot-java.el
@@ -729,8 +729,9 @@ JVM is started in debug mode."
                    (mapconcat #'identity eglot-java-run-test-jvm-args " ")
                    " -cp "
                    "\""
-                   (mapconcat #'expand-file-name cp path-separator) path-separator
                    (expand-file-name eglot-java-junit-platform-console-standalone-jar)
+                   path-separator
+                   (mapconcat #'expand-file-name cp path-separator)
                    "\""
                    " org.junit.platform.console.ConsoleLauncher"
                    " execute"
